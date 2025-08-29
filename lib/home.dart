0971eb87690fbe93f2ae85409bc25db6aa421867
Vysakhchandren <vysakhc14@gmail.com>
2025-08-30 00:51:24 +0530

    Refactored movie card and added movie details page

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:movie_apps/models/movie.dart';
+import 'package:movie_apps/pages/movie_details.dart';
 import 'package:movie_apps/providers/movie_provider.dart';
 import 'package:movie_apps/utils/movie_parser.dart';
 import 'package:provider/provider.dart';
+
+import 'components/movie_card.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -28,49 +32,12 @@ class _HomeState extends State<Home> {
           itemCount: movies.movieList.length,
           itemBuilder: (context, index) {
             final movie = movies.movieList[index];
-            return Card(
-              child: ExpansionTile(
-                title: Text(movie.title),
-                subtitle: Text('Director: ${movie.director}'),
-                leading: CircleAvatar(child: Text(movie.title[0])),
-                children: [
-                Container(
-                alignment: Alignment.center,
-                padding:
-                EdgeInsets.only(left: 75),
-                child: Column(
-                  children: [
-                    RichText(text: TextSpan(
-                      style: DefaultTextStyle
-                          .of(context)
-                          .style,
-                      children: [
-                        TextSpan(text: 'Released:',
-                            style: Theme.of(context).textTheme.labelLarge?.copyWith(fontWeight: FontWeight.bold)
-                        ),
-                        TextSpan(text: '${movie.released}')
-                      ],
-                    ),
-                    )
-                  ],
-                ),
-              ),
-                ],
-            ),
-            );
-            // return ListTile(
-            //   title: Text(movie.title),
-            //   subtitle: Text(movie.director),
-            //   trailing: Icon(Icons.arrow_circle_right),
-            //   leading: CircleAvatar(foregroundImage: NetworkImage(movie.images[0]),
-            //   ),
-            //     // child: Image.network(fit: BoxFit.cover,movie.images[0]),
-            //   // ),
-            //
-            // );
+            return MovieCard(movie: movie);
           },
         ),
       ),
     );
   }
 }
+
+
